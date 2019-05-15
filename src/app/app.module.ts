b553ff5a2f595b2889a1756565d493ce0cb66667
taisa <taisaslins@gmail.com>
2019-05-15 17:22:59 -0300

    enter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,7 @@ import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
+import { SplashScreen } from '@ionic-native/splash-screen'; 
 
 @NgModule({
   declarations: [
